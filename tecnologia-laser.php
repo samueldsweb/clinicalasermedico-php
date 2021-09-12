cf4a88f82b983ce04e4f39ae1dfc72a748114fb2
Samuel Sánchez <samueldsweb@gmail.com>
2021-09-12 03:17:13 -0300

    Se agregan maquinas laser en nueva seccion

--- tecnologia-laser.php
+++ tecnologia-laser.php
@@ -1,16 +1,117 @@
 <!DOCTYPE HTML>
 <html lang="es">
-    <?php 
-    $title = "Tecnología Láser - Clínica Láser Médico";
-    $description = "Contamos con la tecnología láser más avanzada del país.";
-    $keywords = "Tratamiento Láser, Depilación Láser, Tratamiento Láser para el acné, Tratamiento Láser para manchas, Tratamiento Láser para bajar de peso, Tratamiento Láser para la cara, Tratamiento Láser precio.";
-    include("head.php");
-    ?>
+<?php
+$title = "Tecnología Láser - Clínica Láser Médico";
+$description = "Contamos con la tecnología láser más avanzada del país.";
+$keywords = "Tratamiento Láser, Depilación Láser, Tratamiento Láser para el acné, Tratamiento Láser para manchas, Tratamiento Láser para bajar de peso, Tratamiento Láser para la cara, Tratamiento Láser precio.";
+include("head.php");
+?>
+
 <body>
     <?php include("header.php") ?>
-    <?php $title_banner = "Tecnología Láser"; include("breadcumb.php") ?>
+    <?php $title_banner = "Tecnología Láser";
+    include("breadcumb.php") ?>
+
+    <section>
+        <!-- clm_project_area -->
+        <article class="clm_project_area" id="project">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="witr_section_title">
+                        <div class="witr_section_title_inner text-center">
+                            <h2> Contamos con la última</h2>
+                            <h1>Tecnología Láser</h1>
+                        </div>
+                    </div>
+                    <div class="container my-5">
+                        <p> La unión entre la ciencia y la tecnología en atención a la salud están para resolver problemas. El láser se ha transformado por su desarrollo tecnológico en un equipo único en el servicio de la salud tanto pública como privada.
+                            En la medicina estética, los láseres nos permiten obtener precisión, seguridad y rápida recuperación. Además de cicatrización óptima, menor tiempo de inactividad y mayor comodidad para el paciente.
+                            En el centro de salud Clínica Láser Médico, estamos preparados para realizar todos los tratamientos médico estéticos más importantes.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="container">
+            <div class="row">
+                <!-- clm_process_01 -->
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="witr_process all_process_color">
+                        <div class="witr_front_content">
+                            <div class="witr_process_image">
+                                <img src="assets/images/laser-diodo.jpg" alt="Láser Diodo">
+                                <div class="witr_back_process">
+                                    <div class="witr_content_service">
+                                        <h3><a href="#">Láser Diodo</a></h3>
+                                        <p>La luz láser puede alcanzar de forma selectiva las estructuras más profundas de la piel. Los folículos pilosos, el vello grueso y profundo pueden ser eliminados. Con la tecnología láser Diodo podemos realizar una depilación con un 90% de éxito. Este laser elimina el vello de manera segura y rápida, sin dejar quemaduras.</p>
+                                    </div>
+                                </div>
+                                <div class="witr_process_box">
+                                    <div class="witr_process_icon">
+                                        <i class="fas fa-plus"></i>
+                                    </div>
+                                    <div class="witr_process_icon2">
+                                        <i class="fas fa-minus"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- clm_process_02 -->
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="witr_process all_process_color">
+                        <div class="witr_front_content">
+                            <div class="witr_process_image">
+                                <img src="assets/images/laser-neodimio-yag.jpg" alt="Láser Neodimio Yag">
+                                <div class="witr_back_process">
+                                    <div class="witr_content_service">
+                                        <h3><a href="#">Láser Neodimio Yag</a></h3>
+                                        <p>Podemos alcanzar las venas en las lesiones vasculares mediante el Laser de Neodimio Yag. Esta tecnología láser estética también es capaz de depilar, tratar la foliculitis y la despigmentación.</p>
+                                    </div>
+                                </div>
+                                <div class="witr_process_box">
+                                    <div class="witr_process_icon">
+                                        <i class="fas fa-plus"></i>
+                                    </div>
+                                    <div class="witr_process_icon2">
+                                        <i class="fas fa-minus"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- clm_process_03 -->
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="witr_process all_process_color">
+                        <div class="witr_front_content">
+                            <div class="witr_process_image">
+                                <img src="assets/images/laser-co2-fraccionado.jpg" alt="Láser Co2 fraccionado">
+                                <div class="witr_back_process">
+                                    <div class="witr_content_service">
+                                        <h3><a href="#">Láser Co2 fraccionado</a></h3>
+                                        <p>Las arrugas, las manchas faciales, las cicatrices, el acné y sus secuelas, las líneas de expresión son eliminadas a través del láser de Co2 fraccionado. Con este láser también se consigue realizar un rejuvenecimiento facial y del contorno de ojos.</p>
+                                    </div>
+                                </div>
+                                <div class="witr_process_box">
+                                    <div class="witr_process_icon">
+                                        <i class="fas fa-plus"></i>
+                                    </div>
+                                    <div class="witr_process_icon2">
+                                        <i class="fas fa-minus"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
 
     <?php include("footer.php") ?>
     <?php include("scripts.php") ?>
 </body>
+
 </html>
